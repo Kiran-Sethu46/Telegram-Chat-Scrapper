4b6a1585ab36985abec1a29caf4a535274c9cfc9
kiran-sethu46 <kiran226242@gmail.com>
2021-07-21 02:32:31 +0530

    :fire:done

---
+++ message.py
@@ -0,0 +1,52 @@
+from telethon.sync import TelegramClient
+from telethon.tl.functions.messages import GetDialogsRequest
+from telethon.tl.functions.messages import GetHistoryRequest
+from telethon.tl.types import InputPeerEmpty
+
+
+api_id =  #your api_id without '' 
+api_hash = '------------------'#your api_hash
+phone = '+91---------'# your phonenumber with international code 
+client = client = TelegramClient('GetHistoryRequest',api_id, api_hash)
+
+client.connect()
+if not client.is_user_authorized():
+    client.send_code_request(phone)
+    client.sign_in(phone, input('Enter the code: '))
+
+
+chats = []
+last_date = None
+chunk_size = 200
+groups=[]
+result = client(GetDialogsRequest(
+             offset_date=last_date,
+             offset_id=0,
+             offset_peer=InputPeerEmpty(),
+             limit=chunk_size,
+             hash = 0
+         ))
+chats.extend(result.chats)
+
+for chat in chats:
+    try:
+        if chat.megagroup== True:
+            groups.append(chat)
+    except:
+        continue
+
+print('Choose a group to scrape members from:')
+i=0
+for g in groups:
+    print(str(i) + '- ' + g.title)
+    i+=1
+
+g_index = input("Enter a Number: ")
+target_group=groups[int(g_index)]
+
+
+print('Fetching data...')
+
+for message in client.get_messages(target_group,limit=1000):
+    print(message.message)
+    
